185e736e9e6265438f0d52637522dfcec8239310
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2023-06-14 09:01:38 +0300

    lemme cook

---
+++ Attributes&Properties/app1.js
@@ -0,0 +1,7 @@
+// JavaScript object with properties
+const person = {
+    name: 'Ruperth',
+    age: 22
+};
+
+console.log(person.name); // Output: Ruperth
---
+++ Attributes&Properties/app2.js
@@ -0,0 +1,6 @@
+// HTML element with attributes
+const element = document.createElement('div');
+element.setAttribute('id', 'myDiv');
+
+console.log(element.id); // Output: myDiv
+console.log(element.getAttribute('id')); // Output: myDiv
